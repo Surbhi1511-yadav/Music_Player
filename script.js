0d5108f932a5a7249816e569e79ea3c2543e8394
Surbhi1511-yadav <surbhi.yadav_cs19@gla.ac.in>
2022-02-28 09:54:11 +0530

    commits

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const music = document.querySelector("audio");
 const play = document.getElementById("play");
+
+//to play music by clicking on play button
+play.addEventListener("click", () => {
+    music.play();
+});
